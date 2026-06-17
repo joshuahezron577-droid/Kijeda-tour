2cd56a6c221bfee8e2ec8b01b450e1d70ce7a20a
joshuahezron577-android <joshuahezron577@gmail.com>
2026-06-17 11:24:37 +0300

    we are start

--- app/layout.js
+++ app/layout.js
@@ -13,19 +13,7 @@ export default function RootLayout({ children }) {
 {/* Footer */}
 
 {/* Third party scripts */}
- <Script id="tawk-script" strategy="afterInteractive">
-    {`
-      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-      (function(){
-      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-      s1.async=true;
-      s1.src='https://embed.tawk.to/693347b229e54f197c444fba/1jbo504ou';
-      s1.charset='UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1,s0);
-      })();
-    `}
-  </Script>
+ 
 
       </body>
     </html>
